c1ac70516e48f85c13d86543682de4f8a2c04e7a
Khagesh Sharma <khageshhiet@gmail.com>
2021-06-07 16:00:45 +0530

    Try a fix for test flight connectme crash

---
+++ ios/do-not-delete-file-only-for-runtime-library.swift
@@ -0,0 +1,9 @@
+//
+//  do-not-delete-file-only-for-runtime-library.swift
+//  ConnectMe
+//
+//  Created by evernym on 04/06/21.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+import Foundation
