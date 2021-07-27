0368e81ae1c2ec1870b2f7ee7d646e31ccd207d1
Dmitry Yurov <dmitry.yurov@evernym.com>
2021-07-27 13:03:03 +0300

    Change paths

---
+++ e2e-automation/src/test/java/Tests/InstallAppFromAppCenterTest.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import appModules.AppCenterAPI;
+import org.testng.annotations.Test;
+import test.java.utility.Config;
+import test.java.utility.IntSetup;
+
+
+public class InstallAppFromAppCenterTest extends IntSetup {
+
+    @Test
+    public void installRcFromAppCenter() throws Exception {
+        String appPath = AppCenterAPI.downloadRelevantApp(Config.Device_Type);
+        driverApp.installApp(appPath);
+    }
+}
---
+++ e2e-automation/src/test/java/Tests/InstallBuildArtifactAppTest.java
@@ -0,0 +1,24 @@
+package Tests;
+
+import org.testng.annotations.Test;
+import test.java.utility.Config;
+import test.java.utility.IntSetup;
+
+import java.util.Locale;
+
+
+public class InstallBuildArtifactAppTest extends IntSetup {
+    public static String apkPath = "android/app/build/outputs/apk/release/app-arm64-v8a-release.apk";
+    public static String ipaPath = "ios/ConnectMe.ipa";
+
+    @Test
+    public void installBuildArtifact() throws Exception {
+      if(Config.Device_Type.toLowerCase(Locale.ROOT).contains("android")) {
+        driverApp.installApp(apkPath);
+      }
+      else if (Config.Device_Type.toLowerCase(Locale.ROOT).contains("ios")) {
+        driverApp.installApp(ipaPath);
+      }
+    }
+
+}
