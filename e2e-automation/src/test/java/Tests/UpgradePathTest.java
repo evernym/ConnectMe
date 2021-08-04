12dd533593decd2c181e7da5f882a23118eaf747
Dmitry Yurov <dmitry.yurov@evernym.com>
2021-08-04 12:13:34 +0300

    Cleanup test cases, rollback test settings

--- e2e-automation/src/test/java/Tests/UpgradePathTest.java
+++ e2e-automation/src/test/java/Tests/UpgradePathTest.java
@@ -1,5 +1,8 @@
 package test.java.Tests;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -15,9 +18,100 @@ public class UpgradePathTest extends IntSetup {
     private final String oobInvitation = "out-of-band-invitation-upt";
 
     @BeforeClass
-    public void BeforeClassSetup() {
-        System.out.println("Connection Test Suite has been started!");
-        driverApp.launchApp();
+    public void BeforeClassSetup() throws Exception {
+        passCodePageNew.openApp();
+    }
+
+    // This part of UPT is identical to the AppElementTest class
+    @Test
+    public void checkHome() {
+        homePageNew.checkHome();
+    }
+
+    @Test(dependsOnMethods = "checkHome")
+    public void checkMenu() throws InterruptedException {
+        homePageNew.tapOnBurgerMenu();
+        menuPageNew.menuContainer.isDisplayed();
+        menuPageNew.banner.isDisplayed();
+        menuPageNew.logo.isDisplayed();
+        menuPageNew.builtByFooter.isDisplayed();
+        menuPageNew.versionFooter.isDisplayed();
+
+        menuPageNew.userAvatar.click();
+        if (!test.java.appModules.AppUtils.isElementAbsent(driverApp, menuPageNew.okButton)) {
+            menuPageNew.okButton.click();
+            menuPageNew.menuAllowButton.click();
+        } else {
+            System.out.println("Permissions already have been granted!");
+        }
+
+        Thread.sleep(1000);
+        if ((test.java.utility.Config.Device_Type.equals("android") || test.java.utility.Config.Device_Type.equals("awsAndroid"))) {
+            ((AndroidDriver) driverApp).pressKey(new KeyEvent(AndroidKey.BACK));
+        } else {
+            menuPageNew.cancelButton.click();
+        }
+
+        menuPageNew.homeButton.click();
+
+        homePageNew.tapOnBurgerMenu();
+        menuPageNew.myConnectionsButton.click();
+        myConnectionsPageNew.myConnectionsHeader.isDisplayed();
+        homePageNew.scanButton.isDisplayed();
+
+        homePageNew.tapOnBurgerMenu();
+        menuPageNew.myCredentialsButton.click();
+        myCredentialsPageNew.myCredentialsHeader.isDisplayed();
+        homePageNew.scanButton.isDisplayed();
+
+        homePageNew.tapOnBurgerMenu();
+        menuPageNew.settingsButton.click();
+
+        homePageNew.tapOnBurgerMenu();
+        menuPageNew.homeButton.click();
+        homePageNew.homeHeader.isDisplayed();
+    }
+
+    @Test(dependsOnMethods = "checkMenu")
+    public void checkQrScanner() throws Exception {
+        homePageNew.scanButton.isDisplayed();
+        homePageNew.scanButton.click();
+
+        if (!test.java.appModules.AppUtils.isElementAbsent(driverApp, qrScannerPageNew.scannerAllowButton)) {
+            qrScannerPageNew.scannerAllowButton.click();
+        } else {
+            System.out.println("Permissions already have been granted!");
+        }
+
+        Thread.sleep(1000);
+        qrScannerPageNew.scannerCloseButton.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(dependsOnMethods = "checkQrScanner")
+    public void checkSettings() throws Exception {
+        homePageNew.tapOnBurgerMenu(); // go to Menu
+        menuPageNew.settingsButton.click(); // go to Settings
+
+        settingsPageNew.settingsContainer.isDisplayed();
+        settingsPageNew.settingsHeader.isDisplayed();
+        settingsPageNew.biometricsButton.click();
+        settingsPageNew.passCodeButton.click();
+        passCodePageNew.backArrow.click();
+        settingsPageNew.chatButton.click();
+        Thread.sleep(1000);
+        try {
+            chatPageNew.backArrow.click();
+        } catch (Exception e) {
+            if (test.java.utility.Config.iOS_Devices.contains(test.java.utility.Config.Device_Type)) {
+                chatPageNew.backArrowAlt.click();
+            } else {
+                ((AndroidDriver) driverApp).pressKey(new KeyEvent(AndroidKey.BACK));
+            }
+        }
+
+        settingsPageNew.aboutButton.click();
+        aboutPageNew.backArrow.click();
     }
 
     @DataProvider(name = "data1")
@@ -28,6 +122,7 @@ public class UpgradePathTest extends IntSetup {
         };
     }
 
+    // This part of the UPT is identical to the ConnectionTest class
     @Test(dataProvider = "data1")
     public void rejectConnectionTest(String invitationType) throws Exception {
         driverBrowser = test.java.utility.BrowserDriver.getDriver();
