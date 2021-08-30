e12e1fb6dc2e8cafee7df9b439627314db3ca741
Dmitry Yurov <dmitry.yurov@evernym.com>
2021-08-30 14:58:19 +0300

    * Updated capabilities-holding class to reduce duplicated code and improve readability * Added new driver initialization for ios upgrade * Changed UPT helper tests to use new code for ios

--- e2e-automation/src/test/java/Tests/UtilityTests/InstallBuildArtifactAppTest.java
+++ e2e-automation/src/test/java/Tests/UtilityTests/InstallBuildArtifactAppTest.java
@@ -1,18 +1,27 @@
 package test.java.Tests.UtilityTests;
 
 import appModules.AppCenterAPI;
+import org.openqa.selenium.Platform;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import test.java.utility.Config;
 import test.java.utility.IntSetup;
-
+import test.java.utility.AppDriver;
 
 public class InstallBuildArtifactAppTest extends IntSetup {
     @Test
     public void installBuildArtifact() throws Exception {
         try {
             String appPath = AppCenterAPI.getLatestAppDownloadUrl(Config.Device_Type);
-            driverApp.installApp(appPath);
+
+            if(test.java.utility.Helpers.getPlatformType().equals(Platform.ANDROID)) {
+                driverApp.installApp(appPath);
+            }
+            else
+            {
+                // iOS-specific: cannot install app directly
+                driverApp = AppDriver.getIosDriverWithProvidedBundle(appPath);
+            }
         }
         catch (Exception e)
         {
--- e2e-automation/src/test/java/utility/AppDriver.java
+++ e2e-automation/src/test/java/utility/AppDriver.java
@@ -3,8 +3,11 @@ package test.java.utility;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.Reporter;
@@ -21,63 +24,60 @@ public class AppDriver {
     public static final int SMALL_TIMEOUT = 15;
     public static final int SUPER_SMALL_TIMEOUT = 10;
 
+    private static DesiredCapabilities getSharedCapabilitiesForAndroid() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.NO_RESET, true);
+        caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "60000");
+        caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "me.connect");
+        caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".MainActivity");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, Config.Device_Name);
+        return caps;
+    }
+
+    private static DesiredCapabilities getSharedCapabilitiesForIos() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.NO_RESET, true);
+        caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "60000");
+        caps.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.evernym.connectme.callcenter");
+        caps.setCapability(IOSMobileCapabilityType.XCODE_ORG_ID, "ES8QU3D2A4");
+        caps.setCapability(IOSMobileCapabilityType.XCODE_SIGNING_ID, "iPhone Developer");
+        return caps;
+    }
+
     public static AppiumDriver getDriver() {
         if (driver == null) {
             String deviceType = Config.Device_Type;
-
             try {
-                DesiredCapabilities capabilities = new DesiredCapabilities();
+                DesiredCapabilities capabilities;
                 if (deviceType.equals("iOS") || deviceType.equals("iOSSimulator")) {
-                    capabilities.setCapability("platformName", "IOS");
-                    capabilities.setCapability("showXcodeLog", true);
-                    capabilities.setCapability("useNewWDA", true);
+                    capabilities = getSharedCapabilitiesForIos();
+                    capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.IOS);
                     capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
-                    capabilities.setCapability("platformVersion", "14.5");
-                    capabilities.setCapability("deviceName", Config.Device_Name);
-                    capabilities.setCapability("udid", Config.Device_UDID);
-                    capabilities.setCapability("bundleId", "com.evernym.connectme.callcenter");
-                    capabilities.setCapability("xcodeOrgId", "ES8QU3D2A4");
-                    capabilities.setCapability("xcodeSigningId", "iPhone Developer");
-                    capabilities.setCapability("noReset", true);
-                    capabilities.setCapability("newCommandTimeout", "60000");
+                    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.5");
+                    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, Config.Device_Name);
+                    capabilities.setCapability(MobileCapabilityType.UDID, Config.Device_UDID);
+                    capabilities.setCapability(IOSMobileCapabilityType.SHOW_XCODE_LOG, true);
+                    capabilities.setCapability(IOSMobileCapabilityType.USE_NEW_WDA, true);
                     driver = new IOSDriver(new URL(Config.Appium_Server), capabilities);
-                    driver.manage().timeouts().implicitlyWait(LARGE_TIMEOUT, TimeUnit.SECONDS);
                     System.out.println("connectMe application launched successfully in iOS");
                 } else if (deviceType.equals("awsiOS")) {
-                    capabilities.setCapability("platformVersion", "13.0");
-                    capabilities.setCapability("bundleId", "com.evernym.connectme.callcenter");
-                    capabilities.setCapability("xcodeOrgId", "ES8QU3D2A4");
-                    capabilities.setCapability("xcodeSigningId", "iPhone Developer");
-                    capabilities.setCapability("waitForQuiescence", "false");
-                    capabilities.setCapability("shouldUseSingletonTestManager", "true");
-                    capabilities.setCapability("wdaStartupRetryInterval", "1000");
-                    capabilities.setCapability("noReset", true);
-                    capabilities.setCapability("newCommandTimeout", "60000");
+                    capabilities = getSharedCapabilitiesForIos();
+                    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0");
+                    capabilities.setCapability("waitForQuiescence", false);
+                    capabilities.setCapability("shouldUseSingletonTestManager", true);
+                    capabilities.setCapability(IOSMobileCapabilityType.WDA_STARTUP_RETRY_INTERVAL, "1000");
                     driver = new IOSDriver(new URL(Config.Appium_Server), capabilities);
-                    driver.manage().timeouts().implicitlyWait(LARGE_TIMEOUT, TimeUnit.SECONDS);
                     System.out.println("connectMe application launched successfully in iOS");
                 } else if (deviceType.equals("android")) {
-                    capabilities.setCapability("automationName", "UiAutomator2");
-                    capabilities.setCapability("platformName", "Android");
-                    capabilities.setCapability("noReset", "true");
-                    capabilities.setCapability("newCommandTimeout", "60000");
-                    capabilities.setCapability("appPackage", "me.connect");
-                    capabilities.setCapability("appActivity", ".MainActivity");
-                    capabilities.setCapability("deviceName", Config.Device_Name);
+                    capabilities = getSharedCapabilitiesForAndroid();
                     driver = new AndroidDriver(new URL(test.java.utility.Config.Appium_Server), capabilities);
-                    driver.manage().timeouts().implicitlyWait(LARGE_TIMEOUT, TimeUnit.SECONDS);
                     System.out.println("connectMe application launched successfully in android");
                 } else if (deviceType.equals("awsAndroid")) {
-                    capabilities.setCapability("automationName", "UiAutomator2");
-                    capabilities.setCapability("platformName", "Android");
-                    capabilities.setCapability("noReset", "true");
-                    capabilities.setCapability("newCommandTimeout", "60000");
-                    capabilities.setCapability("appPackage", "me.connect");
-                    capabilities.setCapability("appActivity", ".MainActivity");
-                    capabilities.setCapability("deviceName", test.java.utility.Config.Device_Name);
-                    capabilities.setCapability("printPageSourceOnFindFailure", "true");
+                    capabilities = getSharedCapabilitiesForAndroid();
+                    capabilities.setCapability(MobileCapabilityType.PRINT_PAGE_SOURCE_ON_FIND_FAILURE, true);
                     driver = new AndroidDriver(new URL(test.java.utility.Config.Appium_Server), capabilities);
-                    driver.manage().timeouts().implicitlyWait(LARGE_TIMEOUT, TimeUnit.SECONDS);
                     System.out.println("connectMe application launched successfully in android");
                 }
             } catch (Exception e) {
@@ -86,6 +86,24 @@ public class AppDriver {
                 Assert.fail();
             }
         }
+        driver.manage().timeouts().implicitlyWait(LARGE_TIMEOUT, TimeUnit.SECONDS);
+
+        return driver;
+    }
+
+    public static AppiumDriver getIosDriverWithProvidedBundle(String bundlePath)
+    {
+        try {
+            DesiredCapabilities caps = getSharedCapabilitiesForIos();
+            caps.setCapability(MobileCapabilityType.APP, bundlePath);
+            driver = new AndroidDriver(new URL(test.java.utility.Config.Appium_Server), caps);
+        }
+        catch (Exception e) {
+            Reporter.log("Class Setup | Method OpenBrowser | Exception desc : " + e.getMessage());
+            System.out.println("Class Setup | Method OpenBrowser | Exception desc : " + e.getMessage());
+            Assert.fail();
+        }
+        driver.manage().timeouts().implicitlyWait(LARGE_TIMEOUT, TimeUnit.SECONDS);
         return driver;
     }
 
