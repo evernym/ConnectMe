58519aced94ba147982d27c81d56e7183cbe3182
Dmitry Yurov <dmitry.yurov@evernym.com>
2021-10-12 13:13:27 +0300

    Get proper device info

--- e2e-automation/src/test/java/Tests/StartUpTest.java
+++ e2e-automation/src/test/java/Tests/StartUpTest.java
@@ -1,5 +1,6 @@
 package test.java.Tests;
 
+import com.google.common.collect.ImmutableMap;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,6 +18,35 @@ public class StartUpTest extends IntSetup {
 
     @Test
     public void setUpWizardTest() throws Exception {
+        System.out.println(">>>>>>>>>>>>>>>> Device info");
+        try {
+            System.out.println("Device name: " + driverApp.getCapabilities().getCapability("deviceName").toString());
+        }
+        catch (Exception e) {}
+        try {
+            System.out.println("Device model: " + driverApp.getCapabilities().getCapability("deviceModel").toString());
+        }
+        catch (Exception e) {}
+        try {
+            System.out.println("Cap type:version: " + driverApp.getCapabilities().getCapability("CapabilityType.VERSION").toString());
+        }
+        catch (Exception e) {}
+        try {
+            System.out.println("Shell model: " + driverApp.executeScript("mobile: shell", ImmutableMap.of("command", "getprop ro.product.model")).toString());
+        }
+        catch (Exception e) {}
+
+        try {
+            System.out.println("Shell product name: " + driverApp.executeScript("mobile: shell", ImmutableMap.of("command", "getprop ro.product.name")).toString());
+        }
+        catch (Exception e) {}
+
+        try {
+            System.out.println("Shell product device: " + driverApp.executeScript("mobile: shell", ImmutableMap.of("command", "getprop ro.product.device")).toString());
+        }
+        catch (Exception e) {}
+            System.exit(1);
+
         try {
             startUpPageNew.setUpButton.click();
             for (int i = 0; i < 2; i++) {
