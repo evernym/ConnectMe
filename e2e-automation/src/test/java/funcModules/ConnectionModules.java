235e9d9e40cd2ee4612a7141a11993aae30ac29e
vladimir.shishkin <vladimir.shishkin@evernym.com>
2022-01-14 14:45:49 +0300

    debug ios
    Signed-off-by: vladimir.shishkin <vladimir.shishkin@evernym.com>

--- e2e-automation/src/test/java/funcModules/ConnectionModules.java
+++ e2e-automation/src/test/java/funcModules/ConnectionModules.java
@@ -211,15 +211,13 @@ public class ConnectionModules extends IntSetup {
     public void acceptPushNotificationRequest(AppiumDriver driverApp) {
         if (Config.iOS_Devices.contains(Config.Device_Type)) {
 //            if(!AppUtils.isElementAbsent(driverApp, pushNotificationsPageNew.allowButton))
-            try
-            {
+//            {
                 pushNotificationsPageNew.allowButton.click();
                 pushNotificationsPageNew.okButton.click();
-            }
+//            }
 //            else {
-            catch (Exception ex) {
-                System.out.println("Permissions already have been granted!");
-            }
+//                System.out.println("Permissions already have been granted!");
+//            }
         }
     }
 
