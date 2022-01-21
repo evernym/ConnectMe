ff4d2da6eed7260f78c79ca7dd6fa735549b42ed
vladimir.shishkin <vladimir.shishkin@evernym.com>
2022-01-21 14:58:48 +0300

    remove drivers reloading
    Signed-off-by: vladimir.shishkin <vladimir.shishkin@evernym.com>

--- e2e-automation/src/test/java/Tests/StartUpTest.java
+++ e2e-automation/src/test/java/Tests/StartUpTest.java
@@ -10,7 +10,7 @@ public class StartUpTest extends IntSetup {
     @BeforeClass
     public void BeforeClassSetup() {
         System.out.println("Start Up Test Suite has been started!");
-        reloadDriversAndPos(); // for upgrade path test
+//        reloadDriversAndPos(); // for upgrade path test
         driverApp.launchApp();
     }
 
