57fea7305dc3c2c0abcdc7fd1ed64462cf130b5c
vladimir.shishkin <vladimir.shishkin@evernym.com>
2022-01-21 11:52:48 +0300

    try to install RC another way at the beginning
    Signed-off-by: vladimir.shishkin <vladimir.shishkin@evernym.com>

--- e2e-automation/src/test/java/Tests/UtilityTests/InstallAppFromAppCenterTest.java
+++ e2e-automation/src/test/java/Tests/UtilityTests/InstallAppFromAppCenterTest.java
@@ -1,6 +1,7 @@
 package test.java.Tests.UtilityTests;
 
 import appModules.AppCenterAPI;
+import io.appium.java_client.android.Activity;
 import org.openqa.selenium.Platform;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,16 +10,24 @@ import test.java.utility.IntSetup;
 import test.java.utility.Helpers;
 import test.java.utility.AppDriver;
 
+import java.util.HashMap;
+
 public class InstallAppFromAppCenterTest extends IntSetup {
+
+    private static final String APP_PKG = "me.connect";
+    private static final String APP_ACT = ".MainActivity";
+    private static final String BUNDLE_ID = "com.evernym.connectme.callcenter";
 
     @Test
     public void installRcFromAppCenter() throws Exception {
         driverApp.launchApp();
-        if(Helpers.getPlatformType().equals(Platform.ANDROID)) {
-            driverApp.removeApp("me.connect");
-        } else {
-            driverApp.removeApp("com.evernym.connectme.callcenter");
-        }
+
+        // move all logic below without removing app
+//        if(Helpers.getPlatformType().equals(Platform.ANDROID)) {
+//            driverApp.removeApp("me.connect");
+//        } else {
+//            driverApp.removeApp("com.evernym.connectme.callcenter");
+//        }
 
         try {
             String appPath = AppCenterAPI.getReleaseCandidateAppDownloadUrl(Config.Device_Type);
@@ -26,16 +35,33 @@ public class InstallAppFromAppCenterTest extends IntSetup {
 
             if(Helpers.getPlatformType().equals(Platform.ANDROID)) {
                 driverApp.installApp(appPath);
+                // +
+                Activity activity = new Activity(APP_PKG, APP_ACT);
+                driverApp.startActivity(activity);
             }
             else
             {
-                // iOS-specific: cannot install app directly
-                driverApp = AppDriver.getIosDriverWithProvidedBundle(appPath);
+//                // iOS-specific: cannot install app directly
+//                driverApp = AppDriver.getIosDriverWithProvidedBundle(appPath);
+
+                HashMap<String, String> bundleArgs = new HashMap<>();
+                bundleArgs.put("bundleId", BUNDLE_ID);
+                driverApp.executeScript("mobile: terminateApp", bundleArgs);
+
+                HashMap<String, String> installArgs = new HashMap<>();
+                installArgs.put("app", appPath);
+                driverApp.executeScript("mobile: installApp", installArgs);
+
+                driverApp.executeScript("mobile: launchApp", bundleArgs);
             }
         }
         catch (Exception e)
         {
             Assert.fail(e.getMessage());
+        }
+        finally {
+            Thread.sleep(5000);
+            driverApp.closeApp();
         }
     }
 }
