e0e335854bf544061b097ebc5dec45e8be02e7b7
artem.ivanov <artem.ivanov@evernym.com>
2022-02-09 13:37:10 +0300

    Updated regression scripts

--- regression-testing/faber.py
+++ regression-testing/faber.py
@@ -19,19 +19,19 @@ from vcx.state import State, ProofState
 # logging.basicConfig(level=logging.DEBUG)
 
 provisionConfig = {
-    'agency_url': 'https://eas01.pps.evernym.com',
-    'agency_did': 'UNM2cmvMVoWpk6r3pG5FAq',
-    'agency_verkey': 'FvA7e4DuD2f9kYHq6B3n7hE7NQvmpgeFRrox3ELKv9vX',
+    'agency_url': 'https://eas.pdev.evernym.com',
+    'agency_did': 'LTjTWsezEmV4wJYD5Ufxvk',
+    'agency_verkey': 'BcCSmgdfChLqmtBkkA26YotWVFBNnyY45WCnQziF4cqN',
     'wallet_name': 'faber_wallet',
     'wallet_key': '123',
-    'enterprise_seed': '000000000000000000000000Trustee2',
+    'enterprise_seed': '000000000000000000000000Trustee1',
     'protocol_type': '3.0',
     'name': 'Faber',
     'logo': 'https://s3.us-east-2.amazonaws.com/public-demo-artifacts/demo-icons/cbFaber.png',
-    'pool_networks': [
+    'indy_pool_networks': [
         {
-            'genesis_path': 'genesis_staging.txn',
-            'namespace_list': ["staging"],
+            'genesis_path': 'genesis.txn',
+            'namespace_list': ["builder"],
             'taa_config': {
                 'taa_digest': '8cee5d7a573e4893b08ff53a0761a22a1607df3b3fcd7e75b98696c92879641f',
                 'acc_mech_type': 'at_submission',
