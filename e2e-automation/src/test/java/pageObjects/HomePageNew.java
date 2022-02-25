4bde991a4c0a405f62b83b12e7fb7963d0b2ca0c
igor shabanov <igor.shabanov@evernym.com>
2022-02-25 16:34:18 +0300

    Changed locator

--- e2e-automation/src/test/java/pageObjects/HomePageNew.java
+++ e2e-automation/src/test/java/pageObjects/HomePageNew.java
@@ -53,14 +53,14 @@ public class HomePageNew {
 
     //""Making secure connection..." Other"
 
-//    @AndroidFindBy(xpath = "//android.widget.TextView[contains(text(), \"Making secure connection\")]")
-//    @AndroidFindBy(xpath = "//android.widget.TextView[contains(text(), 'Making secure connection')]")
+    @AndroidFindBy(xpath = "//android.widget.TextView[contains(text(), \"Making secure connection\")]")
+    @AndroidFindBy(xpath = "//android.widget.TextView[contains(text(), 'Making secure connection')]")
 
 //    @AndroidFindBy(xpath = "//*[@text='Making secure connection...']")
 
 //android.view.ViewGroup[@content-desc="home-container"]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView[1]
 
-//    @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"home-container\"]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView[1]")
+    @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"home-container\"]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView[1]")
 
 //    @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"home-container\"]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView[1]")
 //    @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"home-container\"]//android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView[1]")
