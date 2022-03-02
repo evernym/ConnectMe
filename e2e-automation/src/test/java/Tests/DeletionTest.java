77c2b333a705f7335dd448dcd7e4c1f7f7e610a1
igor shabanov <igor.shabanov@evernym.com>
2022-03-02 14:01:46 +0300

    Refactored: deleteCredentialFromExistingConnection test

--- e2e-automation/src/test/java/Tests/DeletionTest.java
+++ e2e-automation/src/test/java/Tests/DeletionTest.java
@@ -1,5 +1,9 @@
 package test.java.Tests;
 
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -12,6 +16,7 @@ import test.java.utility.LocalContext;
 import test.java.utility.Config;
 import test.java.funcModules.ConnectionModules;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 
@@ -48,39 +53,83 @@ public class DeletionTest extends IntSetup {
             } catch (Exception e) { }
         }
 
-        Thread.sleep(5000); // FIXME
+//        Thread.sleep(5000); // FIXME
+
+//        new WebDriverWait(driverApp, 10, 1000)
+//            .until(ExpectedConditions.visibilityOf(myConnectionsPageNew.myConnectionsHeader));
+//            .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//XCUIElementTypeStaticText[@name=\"My Connections\"]")));
         Assert.assertNull(myConnectionsPageNew.getConnectionByName(connectionInvitation));
     }
 
     @Test(dependsOnMethods = "deleteEmptyConnection")
     public void deleteCredentialFromExistingConnection() throws Exception {
+        WebDriverWait wait = new WebDriverWait(driverApp, 3, 500);
         homePageNew.tapOnBurgerMenu();
         menuPageNew.myCredentialsButton.click();
-        // TODO: move this logic to helper
-        int credsCountBefore = 0;
-        try {
-            objAppUtils.findParameterizedElementAlt(credentialNameManyScheme).click();
-            credsCountBefore = 1;
-            Thread.sleep(1000);
-            if(AppUtils.isElementAbsent(driverApp, connectionHistoryPageNew.threeDotsButton)) throw new NoSuchElementException();
-        } catch (Exception ex) {
-            AppUtils.pullScreenUp(driverApp);
-            credsCountBefore = myCredentialsPageNew.getConnectionsBySchemeName(credentialNameManyScheme).size();
-            objAppUtils.findParameterizedElementAlt(credentialNameManyScheme).click();
-        }
-        connectionHistoryPageNew.threeDotsButton.click();
-        Thread.sleep(1000);
-        credentialPageNew.deleteButton.click();
 
-        if (Config.iOS_Devices.contains(Config.Device_Type)) { // delete button tapping ios issue
+        // === the code that does not work yet
+        // seems to work fine for iOS
+        List<WebElement> credentialsBefore = myCredentialsPageNew.getCredentialsBySchemeName(credentialNameManyScheme);
+        myCredentialsPageNew.expandCredentialBySchemeName(credentialNameScheme);
+//        if (credentialsBefore.size() > 0) {
+//            credentialsBefore.get(0).click();
+//        } else {
+//            throw new Exception("No credentials with name " + credentialNameScheme + " has been found");
+//            // not really needed; the test will fail if three dots are not found
+//            // if no credentials, that means pre-test failed, not the test.
+//        }
+//        if (credentialsBefore.size() > 1) {
+//            credentialsBefore.get(0).click();
+//        }
+
+        wait
+            .until(ExpectedConditions.elementToBeClickable(connectionHistoryPageNew.threeDotsButton))
+            .click();
+        wait
+            .until(ExpectedConditions.elementToBeClickable(credentialPageNew.deleteButton))
+            .click();
+        if (test.java.utility.Helpers.getPlatformType().equals(Platform.IOS)) { // delete button tapping ios issue
             try {
                 credentialPageNew.deleteButton.click();
-            } catch (Exception e) {
-
+            } catch (Exception e) { // got here for the last test run
+                System.out.println("DeletionTest >" +
+                    " deleteCredentialFromExistingConnection >" +
+                    " iOS delete button tapping issue failed");
             }
         }
+        List<WebElement> credentialsAfter = myCredentialsPageNew.getCredentialsBySchemeName(credentialNameManyScheme);
+        Assert.assertEquals(credentialsAfter.size(), credentialsBefore.size() - 1);
+        // ^^^ the code that does not work yet
 
-        Assert.assertEquals(myCredentialsPageNew.getConnectionsBySchemeName(credentialNameManyScheme).size(), 0);
+        // TODO: move this logic to helper (to credential page? - no, to app utils; may cause exception if invoked in the wrong place)
+        //why app utils is not static?
+
+        //=====the code to refactor that works====
+//        int credsCountBefore = 0
+//        try {
+//            objAppUtils.findParameterizedElementAlt(credentialNameManyScheme).click();
+//            credsCountBefore = 1;
+//            Thread.sleep(1000);
+//            if(AppUtils.isElementAbsent(driverApp, connectionHistoryPageNew.threeDotsButton)) throw new NoSuchElementException();
+//        } catch (Exception ex) {
+//            AppUtils.pullScreenUp(driverApp);
+//            credsCountBefore = myCredentialsPageNew.getConnectionsBySchemeName(credentialNameManyScheme).size();
+//            objAppUtils.findParameterizedElementAlt(credentialNameManyScheme).click();
+//        }
+//        connectionHistoryPageNew.threeDotsButton.click(); //why the button is clicked on connectionHistory is a connection has alreqdy been clicked, current page - credential page?
+//        Thread.sleep(1000);
+//        credentialPageNew.deleteButton.click();
+//
+//        if (Config.iOS_Devices.contains(Config.Device_Type)) { // delete button tapping ios issue
+//            try {
+//                credentialPageNew.deleteButton.click();
+//            } catch (Exception e) {
+//
+//            }
+//        }
+//        Assert.assertEquals(myCredentialsPageNew.getConnectionsBySchemeName(credentialNameManyScheme).size(), 0);
+        //^^^^the code to refactor that works^^^^
+
     }
 
     @Test(dependsOnMethods = "deleteCredentialFromExistingConnection")
@@ -101,8 +150,8 @@ public class DeletionTest extends IntSetup {
 
 //        Thread.sleep(5000); // FIXME
 
-        new WebDriverWait(driverApp, 5, 500)
-            .until(ExpectedConditions.visibilityOf(myConnectionsPageNew.myConnectionsHeader));
+//        new WebDriverWait(driverApp, 5, 500)
+//            .until(ExpectedConditions.visibilityOf(myConnectionsPageNew.myConnectionsHeader));
 
         Assert.assertNull(myConnectionsPageNew.getConnectionByName(oobInvitation));
     }
--- e2e-automation/src/test/java/pageObjects/MyCredentialsPageNew.java
+++ e2e-automation/src/test/java/pageObjects/MyCredentialsPageNew.java
@@ -4,33 +4,62 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
-import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import test.java.utility.Config;
 
 import java.util.List;
 
 public class MyCredentialsPageNew {
-  AppiumDriver driver;
+    AppiumDriver driver;
 
-  public MyCredentialsPageNew(AppiumDriver driver) {
+    public MyCredentialsPageNew(AppiumDriver driver) {
     PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     this.driver = driver;
-  }
+    }
 
-  @AndroidFindBy(xpath = "//*[@text=\"My Credentials\"]")
-  @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@name=\"My Credentials\"]")
-  public WebElement myCredentialsHeader;
+    @AndroidFindBy(xpath = "//*[@text=\"My Credentials\"]")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@name=\"My Credentials\"]")
+    public WebElement myCredentialsHeader;
 
-  @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"burger-menu\"]")
-  @iOSXCUITFindBy(xpath = "//XCUIElementTypeOther[@name=\"burger-menu\"]")
-  public WebElement burgerMenuButton;
+    @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"burger-menu\"]")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeOther[@name=\"burger-menu\"]")
+    public WebElement burgerMenuButton;
 
-  public List<WebElement> getConnectionsBySchemeName(String schemeName)
-  {
+    public void expandCredentialBySchemeName(String credentialNameScheme) throws NoSuchElementException {
+        // TODO: implement scroll-searching if needed
+        List<WebElement> credentials = getCredentialsBySchemeName(credentialNameScheme);
+        if (credentials.size() > 0) {
+            credentials.get(0).click();
+        } else {
+            throw new NoSuchElementException("No credentials with scheme name " + credentialNameScheme + " found");
+        }
+        if (credentials.size() > 1) {
+            credentials.get(0).click();
+        }
+    }
+
+    public List<WebElement> getConnectionsBySchemeName(String schemeName) {
       String locator = Config.iOS_Devices.contains(Config.Device_Type)?
             "//*[@label='" + schemeName + "']" : "//*[@label='" + schemeName + "']";
       return (List<WebElement>)driver.findElementsByXPath(locator);
-  }
+    }
+
+    public List<WebElement> getCredentialsBySchemeName(String schemeName) {
+        // TODO: implement scroll-searching
+
+    //      new WebDriverWait(driver, 3, 500)
+    //          .until(ExpectedConditions.visibilityOf(myCredentialsHeader));
+        List<WebElement> credentials;
+        if (test.java.utility.Helpers.getPlatformType().equals(Platform.IOS)) {
+            credentials = driver.findElementsByAccessibilityId(schemeName + "-title");
+        } else {
+            credentials = driver.findElementsById(schemeName + "-title");
+        }
+         return credentials;
+    }
 }
